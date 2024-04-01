3a6452e70e0560fb5a4e78fc421c09b918e89275
Kristian2705 <k_petrov27@abv.bg>
2024-04-01 22:22:53 +0300

    Initial

--- BetaTesters/Program.cs
+++ BetaTesters/Program.cs
@@ -10,7 +10,6 @@ namespace BetaTesters
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
@@ -22,7 +21,6 @@ namespace BetaTesters
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -31,7 +29,6 @@ namespace BetaTesters
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
