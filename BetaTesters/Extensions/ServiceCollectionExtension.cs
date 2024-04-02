b32c3e8f24125b4f8dc84544b4077aeae26a7c15
Kristian2705 <k_petrov27@abv.bg>
2024-04-02 20:46:04 +0300

    ServiceCollectionExtension added to simplify the Program.cs and removed fake connection string

---
+++ BetaTesters/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace BetaTesters.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            return services;
+        }
+    }
+}
