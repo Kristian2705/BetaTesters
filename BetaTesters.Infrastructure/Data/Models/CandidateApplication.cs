d3005ebb26b3110236d617362c88a3b0368bb8fe
Kristian2705 <k_petrov27@abv.bg>
2024-04-02 23:24:22 +0300

    abc

--- BetaTesters.Infrastructure/Data/Models/CandidateApplication.cs
+++ BetaTesters.Infrastructure/Data/Models/CandidateApplication.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using static BetaTesters.Infrastructure.Constants.DataConstants;
